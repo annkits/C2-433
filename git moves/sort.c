e95a8b6d12c3a7454dcebcfdc583bc3858131a5c
annkits <annpodolskaya0965@gmail.com>
2025-03-08 09:07:20 +0700

    Сообщение коммита: что поменялось

---
+++ git moves/sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define N 100000
+#define MAX_TOTAL 301
+#define CHECK 5
+
+void SelectionSort(struct Student arr[], int size){
+    for (int i = 0; i < size - 1; i++){
+        int j_max = i;
+        for (int j = i + 1; j < size; j++){
+            if (arr[j].total > arr[j_max].total){
+                j_max = j;
+            }
+        }
+        struct Student temp = arr[i];
+        arr[i] = arr[j_max];
+        arr[j_max] = temp;
+        memset(&temp, 0, sizeof(struct Student));
+    }
+}
